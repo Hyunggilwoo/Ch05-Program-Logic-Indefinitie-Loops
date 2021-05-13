7877b71465d4e904c68d286a43393c4d68989828
Hyunggil Woo <hyunggilswoo@gmail.com>
2021-05-13 13:16:52 -0700

    threeHeads test passes and digitSum test passes.

--- Ch05.java
+++ Ch05.java
@@ -10,7 +10,8 @@ import java.util.*;
 public class Ch05{
 	public static void main(String[]args){
 		printFactors(24);
-		threeHeads(52);
+		threeHeads(12345);
+		digitSum(-123456);
 	}
      public static void printFactors(int n){
 		 
@@ -23,9 +24,10 @@ public class Ch05{
 		for (int i = n-1; i >= 1; i--){
 			if ( (n % i) == 0) {
 			System.out.print( " and " + n/i); // Prints "and" and factors of n.
+			
 			}
          }
-         
+         System.out.println();
 	}
 	/*-----------------------------------------------------------------
 	 * The threeHeads turns head if the random number is odd and
@@ -39,17 +41,17 @@ public class Ch05{
 	 * I gained inspiration from https://stackoverflow.com/questions/27917444/how-do-i-create-a-java-random-number-generator-that-creates-3-numbers-using-seed 
 	 * However, I believe I studied in great depth to complete the program by myself.
 	 * 
-	*/-----------------------------------------------------------------
+	-----------------------------------------------------------------*/
 	public static void threeHeads(int seed){
     Random rand = new Random(seed);
     int head = 0; // This is a counter to count the number of head. 
 			while ( head < 3 ){
-				int n = rand.nextInt(99);
+				boolean n = rand.nextBoolean();
 				
-                    if ( (n % 2) > 0) { // flip head if random number is odd
+					if (n == true) { // flip head if random number is even
                         System.out.print("H ");
                         head++;
-                    } else { // flip tail if random number is even
+                    } else { // flip tail if random number is odd
                         System.out.print("T ");
                         head = 0; 
                       }   
@@ -57,20 +59,28 @@ public class Ch05{
 		System.out.println();
 		System.out.print("Three heads in a row!");
 	}
+	/*------------------------------------------------------------------
+	 * I struggled if there was a method for an integer to locate the
+	 * digits possibly converting them into a string, and possibly use a
+	 * similar to charAt() to call on a number similar to it. Then, I 
+	 * got an inspiration from using the same integer.
+	 * 
+	 * 
+	 ----------------------------------------------------------------*/
+	public static int digitSum(int n){
+		
+		int sum = 0;
+		
+		while ( n != 0) {
+			if ( n > 0) {
+				sum += n % 10;
+				n /=10;
+			} else {
+				n = Math.abs( n);
+			}
+				
+		} 
+			return sum;
+	}
 	
-	    //----The code generates a heads or tails.
-    //Random rand = new Random(20);
-    //mystery(rand.nextLine("H H H"));
-    //for(int i =1; i < 10;i++){
-    //System.out.println(rand.nextInt(10));
-    // System.out.print(number);
-
-    // while ( Variable z != "H H H"){
-        // if ( (mystery % 2) > 0) {
-            // System.out.print("H "); }
-        // else {
-            // System.out.print("T "); } 
-    //}
-    // System.out.println();
-    // System.out.print("Three heads in a row!");
 }
